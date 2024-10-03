1e4fd35f3c013d5d64ee022c3590218cc717ff65
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-10-03 07:02:12 +0300

    Reached the infinite loop in calling a function instead of a component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-function Square({ value }){
-	 return <button className="square">{ value }</button>
+import { useState } from 'react';
+
+//Component
+function Square({ value, onSquareClick }){
+	return (
+		<button 
+		className="square"
+		onClick={onSquareClick}
+		>
+		{value}
+		</button>
+	);
 }
 
+//Component
 export default function Board() {
+  const[squares,setSquares] = useState(Array(9).fill(null));
+	
+	function handleClick(index) {
+		const nextSquares = squares.slice();
+		nextSquares[index] = "X";
+		setSquares(nextSquares);
+	}
+
+//Its now being treated as a function rather than a component and hence an infinite loop
   return(
 	  <>
-	  <div className="board-row">
-	  	<Square value="1" />
-	  	<Square value="2" />
-	  	<Square value="3" />
-	  </div>
-	  <div className="board-row">
-	  	<Square value="4" />
-	  	<Square value="5" />
-	  	<Square value="6" />
-	  </div>
-	  <div className="board-row">
-	  	<Square value="7" />
-	  	<Square value="8" />
-	  	<Square value="9" />
-	  </div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
+      </div>
 	  </>
   );
 }
